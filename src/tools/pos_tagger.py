d1c35033c19d61326e5a6b6c341e78c661fc2076
unknown <daniel.gallagher.js@gmail.com>
2024-04-10 14:44:25 +0200

    read data from focloir

--- src/tools/pos_tagger.py
+++ src/tools/pos_tagger.py
@@ -1,33 +1,37 @@
-from tools.lemmatiser import IrishLemmatiser
-import pandas as pd
 from abc import ABC, abstractmethod
+import pandas as pd
+
+IRISH_ADJ_PATH = "data/POS/focloir_adjectives.csv"
+IRISH_NOUN_PATH = "data/POS/focloir_nouns.csv"
+
+def get_lookup_from_focloir(path: str) -> dict:
+    focloir_dataset = pd.read_csv(path)
+    focloir_words = list(focloir_dataset['Item'])
+    lookup = {w:True for w in focloir_words}
+    return lookup
 
 class POSTagger(ABC):
     @abstractmethod
-    def is_adjective(self, lemma: str):
+    def is_POS(lemma: str, pos: str) -> bool:
         pass
 
     @abstractmethod
-    def is_noun(self, lemma: str):
+    def is_adjective(self, lemma: str) -> bool:
         pass
 
     @abstractmethod
-    def is_POS(self, lemma: str, POS: str):
+    def is_noun(self, lemma: str) -> bool:
+        pass
+
+    @abstractmethod
+    def is_POS(self, lemma: str, POS: str) -> bool:
         pass
 
 # this is too specific to the format of the CSV file
 class IrishPOSTagger(POSTagger):
     def __init__(self):
-        self.adj_path = "data/POS/focloir_adjectives.csv"
-        self.noun_path = "data/POS/focloir_nouns.csv"
-
-        adjectives = pd.read_csv(self.adj_path)
-        adjs_as_list = list(adjectives['Item'])
-        adj_lookup = {adj:True for adj in adjs_as_list}
-
-        nouns = pd.read_csv(self.noun_path)
-        nouns_as_list = list(nouns['Item'])
-        noun_lookup = {noun:True for noun in nouns_as_list}
+        adj_lookup = get_lookup_from_focloir(IRISH_ADJ_PATH)
+        noun_lookup = get_lookup_from_focloir(IRISH_NOUN_PATH)
 
         resumptive_pronouns = ['liom']
         resumptive_lookup = {res:True for res in resumptive_pronouns}
@@ -35,7 +39,7 @@ class IrishPOSTagger(POSTagger):
         self.POS_lookup = {
             "ADJ": adj_lookup,
             "NOUN": noun_lookup,
-            "RES": resumptive_pronouns
+            "RES": resumptive_lookup
         }
 
     def is_POS(self, lemma: str, pos: str):
