a196f27308b34c279c600d31b0291dcc2e825365
unknown <daniel.gallagher.js@gmail.com>
2024-04-10 15:21:35 +0200

    resumptive pronoun filtering

--- src/main_a.py
+++ src/main_a.py
@@ -5,10 +5,10 @@ import pandas as pd
 
 tagger = IrishPOSTagger()
 lemmatiser = IrishLemmatiser()
+eos_chars = ['.',',','?','!','-','\'', '>', '<']
 
-def up_to_end_of_sentence(T):
+def up_to_end_of_sentence(T: str) -> str:
     final_string = ""
-    eos_chars = ['.',',','?','!','-','\'']
     for c in T:
         if c not in eos_chars:
             final_string += c
@@ -16,7 +16,17 @@ def up_to_end_of_sentence(T):
             break
     return final_string
 
-def is_comp_preceded_by_noun(row):
+def from_beginning_of_sentence(T: str) -> str:
+    final_string = ""
+    i = len(T)-1
+    while i >= 0:
+        if T[i] in eos_chars:
+            break
+        final_string += T[i]
+        i -= 1
+    return final_string[::-1]
+
+def is_comp_preceded_by_noun(row) -> bool:
     left_of_comp = row['Left'].lower()
     if len(left_of_comp) > 0:
         lemmatised = lemmatiser(left_of_comp)
@@ -24,13 +34,29 @@ def is_comp_preceded_by_noun(row):
         return tagger.is_noun(final_lemma)
     return False
 
+def contains_resumptive(row) -> bool:
+    right_of_comp = row['Right'].lower()
+    if len(right_of_comp) > 0:
+        relevant_expression = up_to_end_of_sentence(right_of_comp)
+        lemmatised = lemmatiser(relevant_expression)
+        for lemma in lemmatised:
+            if tagger.is_resumptive_pronoun(lemma):
+                return True
+        return False
+
 def main():
     df_connacht = pd.read_csv("data/dialect/a_connacht_dataset_100.csv", header=0)
     df_munster = pd.read_csv("data/dialect/a_munster_dataset_100.csv", header=0)
     df_ulster = pd.read_csv("data/dialect/a_ulster_dataset_100.csv", header=0)
     datasets = [df_connacht, df_munster, df_ulster]
+    datasets = {
+        "Connacht": df_connacht,
+        "Munster": df_munster,
+        "Ulster": df_ulster
+    }
 
-    for ds in datasets:
+    for title in datasets.keys():
+        ds = datasets[title]
         ds["full_sentence"] = ds["Left"].str.cat([ds["KWIC"], ds["Right"]], sep=" ")
 
         # Filter to include only a complementisers which
@@ -39,14 +65,28 @@ def main():
         # ds['preceded_by_noun'] = is_noun
         ds = ds[preceded_by_noun]
 
+        # Now we will filter to only find a complementisers
+        # which include a resumptive pronoun anywhere in the
+        # remaining sentence.
+        resumptive_found = ds.apply(contains_resumptive, axis=1)
+        ds = ds[resumptive_found]
+        resumptives_found_as_percent = (resumptive_found.sum() / len(resumptive_found)) * 100
+        print(resumptive_found.sum())
+        print("----")
+        print("Dataset: ", title)
+        print("Number of Resumptives: ", round(resumptives_found_as_percent,2))
+        print("Number of Non Resumptives: ", round(100.0 - resumptives_found_as_percent,2))
+        print("----")
+
+        """
         # We want to only include up to the full stop
         # as we just want the current relevant sentence
         # for the complementiser.
         for s_left, s_right in zip(ds['Left'], ds['Right']):
-            print("----")
-            print(s_left)
-            print(up_to_end_of_sentence(s_right))
-            print("----")
+            print("\n")
+            print(from_beginning_of_sentence(s_left) + " **a** " + up_to_end_of_sentence(s_right))
+            print("\n")
+        """
 
 if __name__ == "__main__":
     main()
