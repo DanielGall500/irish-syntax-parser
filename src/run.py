6c61bca102e4709fa521bf49a8a6a8103efdc16a
unknown <daniel.gallagher.js@gmail.com>
2024-08-29 16:07:57 +0200

    minor changes

--- src/run.py
+++ src/run.py
@@ -1,226 +1,118 @@
-from tools.syntax.complementisers import ComplementiserAnalyser
-from preprocessing.row_manipulation import to_lemmas
-from preprocessing.string_manipulation import from_beginning_of_sentence, up_to_end_of_sentence
-import pandas as pd
+from tools.syntax.parser import SentenceParser, ParsedSentence
+from preprocessing.string_manipulation import json_path_builder
+from collections import deque
 
-ANALYSE_GO = True
-
-if ANALYSE_GO:
-    CONNACHT_DATASET = "data/dialect/go_connacht_dataset_80k.csv"
-    MUNSTER_DATASET = "data/dialect/go_munster_dataset_80k.csv"
-    ULSTER_DATASET = "data/dialect/go_ulster_dataset_80k.csv"
-else:
-    CONNACHT_DATASET = "data/dialect/a_connacht_dataset_100k.csv"
-    MUNSTER_DATASET = "data/dialect/a_munster_dataset_100k.csv"
-    ULSTER_DATASET = "data/dialect/a_ulster_dataset_100k.csv"
-
-comp_analyser = ComplementiserAnalyser()
-
-class ClauseParser:
-    def __init__(self):
+class ExperimentBase:
+    def setup(self):
         pass
 
-    def __call__(self, lemmas: list[str]):
-        all_clauses_analysed = []
-        clauses = comp_analyser.get_comp_clauses(lemmas)
-        clause_as_str = comp_analyser.get_comp_clauses_as_str(lemmas)
-        for clause in clauses:
-            c = clause["clause"]
-            selected_comp = clause["selected_comp"]
-            comp_preceded_by_noun = comp_analyser.clause_ends_in_noun(c)
-            begins_with_adj = comp_analyser.clause_begins_with_adjective(c)
-            begins_with_number = comp_analyser.clause_begins_with_number(c)
-            resumptive_dict = comp_analyser.clause_contains_resumptive(c)
-            resumptive_found = resumptive_dict["found"]
-            resumptive_lemmas = resumptive_dict["lemma"]
+    def run(self):
+        raise NotImplementedError("Subclasses should implement this!")
 
-            clause_info = {
-                "clause": c,
-                "selected_comp": selected_comp,
-                "noun_final": comp_preceded_by_noun,
-                "adj_initial": begins_with_adj,
-                "number_initial": begins_with_number,
-                "is_resumptive_found": resumptive_found,
-                "resumptives": resumptive_lemmas
-            }
-            all_clauses_analysed.append(clause_info)
-        return all_clauses_analysed
+    def teardown(self):
+        pass
 
-class SentenceParser:
-    def __init__(self):
-        self.clause_parser = ClauseParser()
+    def execute(self):
+        self.setup()
+        result = self.run()
+        self.teardown()
+        return result
 
-        # remove this:
-        from tools.morphology.lemmatiser import IrishLemmatiser
-        self.lemmatiser = IrishLemmatiser()
+class AnalyseNounBeforeGoExperiment(ExperimentBase):
+    def __init__(self, sentences):
+        self.sentences = sentences
 
-    def __call__(self, sentence: str):
-        sentence_info = {
-            "full": sentence,
-            "lemmas": None,
-            "clause_structure": None,
-            "num_embedded_clauses": None
+    def run(self):
+        nouns_before_comp_counter = 0
+        total_clauses_counter = 0
+        for sentence in self.sentences:
+            for clause in sentence:
+                if clause["noun_final"]:
+                    nouns_before_comp_counter += 1
+                total_clauses_counter += 1
+
+        percentage = (nouns_before_comp_counter / total_clauses_counter) * 100 if total_clauses_counter > 0 else 0
+
+        return {
+            "Number of complementisers preceded by noun": nouns_before_comp_counter,
+            "Number of clauses": total_clauses_counter,
+            "Percentage": percentage
         }
 
-        lemmas = self.lemmatiser(sentence)
-        clause_structure = self.clause_parser(lemmas)
-        n_clauses = len(clause_structure)
+class AnalyseAExperiment(ExperimentBase):
+    def __init__(self, sentences):
+        self.sentences = sentences
 
-        sentence_info["lemmas"] = lemmas
-        sentence_info["clause_structure"] = clause_structure
-        sentence_info["num_embedded_clauses"] = len(n_clauses)
+    def run(self):
+        res_pronoun_counter = 0
+        n_sentences = len(self.sentences)
 
+        for sentence in self.sentences:
+            n_clauses = sentence.get_num_clauses()
+            if n_clauses > 1:
+                final_clause = deque(sentence, maxlen=1)[0]
+                if final_clause.get("is_resumptive_found", False):
+                    res_pronoun_counter += 1
 
-def format(ds):
-    main_clauses = [from_beginning_of_sentence(x) for x in ds['Left']]
-    embedded_clauses = [up_to_end_of_sentence(x) for x in ds['Right']]
-    comps = ds['KWIC']
-    full_sentences = [main + " " + comp + " " + emb for main, emb, comp in zip(main_clauses, embedded_clauses, comps)]
+        percentage = (res_pronoun_counter / n_sentences) * 100 if n_sentences > 0 else 0
 
+        return {
+            "Number of resumptive final comps found": res_pronoun_counter,
+            "Number of sentences": n_sentences,
+            "Percentage": percentage
+        }
+
+class ExperimentRunner:
+    def __init__(self, experiments):
+        self.experiments = experiments
+
+    def run_all(self):
+        results = {}
+        for experiment in self.experiments:
+            print(f"Running {experiment.__class__.__name__}")
+            result = experiment.execute()
+            results[experiment.__class__.__name__] = result
+        return results
 
 def main():
+    sentence_parser = SentenceParser()
+
     # -- Step 1: Store the data from each region. --
-    df_connacht = pd.read_csv(CONNACHT_DATASET, header=0, nrows=500)
-    df_munster = pd.read_csv(MUNSTER_DATASET, header=0, nrows=500)
-    df_ulster = pd.read_csv(ULSTER_DATASET, header=0, nrows=500)
-    datasets = [df_connacht, df_munster, df_ulster]
-    datasets = {
-        "Connacht": df_connacht,
-        "Munster": df_munster,
-        "Ulster": df_ulster
-    }
+    datasets = [
+        ("go", "Connacht"),
+        ("go", "Munster"),
+        ("go", "Ulster"),
+        ("a", "Connacht"),
+        ("a", "Munster"),
+        ("a", "Ulster")
+    ]
+    for dataset in datasets:
+        comp = dataset[0]
+        region = dataset[1]
+        path = json_path_builder(comp, region)
+        parsed_sentences = sentence_parser.read_from(path)
+        parsed_sentences = [ParsedSentence(sentence) for sentence in parsed_sentences]
 
-    # -- Step 2: Perform pre-processing on each dataset. --
-    for title in datasets.keys():
-        ds = datasets[title]
-        main_clauses = [from_beginning_of_sentence(x) for x in ds['Left']]
-        embedded_clauses = [up_to_end_of_sentence(x) for x in ds['Right']]
-        comps = ds['KWIC']
-
-        full_sentence = [main + " " + comp + " " + emb for main, emb, comp in zip(main_clauses, embedded_clauses, comps)]
-
-        datasets[title]["main_clause"] = main_clauses
-        datasets[title]["embedded_clause"] = embedded_clauses
-        datasets[title]["complementiser"] = comps
-        datasets[title]["full_sentence"] = full_sentence
-
-        linguistic_analysis = run_syntactic_analysis(ds)
-        datasets[title]["followed_by_adj"] = linguistic_analysis["followed_by_adjective"] 
-        datasets[title]["preceded_by_noun"] = linguistic_analysis["preceded_by_noun"] 
-        datasets[title]["followed_by_number"] = linguistic_analysis["followed_by_number"] 
-        datasets[title]["resumptive_found"] = linguistic_analysis["resumptive_found"] 
-        datasets[title]["resumptive"] = linguistic_analysis["resumptive"] 
-
-        print(ds.head())
-
-    # -- Step 3: Run the syntactic analysis. --
-    for title in datasets.keys():
-        ds = datasets[title]
-        followed_by_adj_feature = ds["followed_by_adj"]
-        preceded_by_noun_feature = ds["preceded_by_noun"]
-        followed_by_number_feature = ds["followed_by_number"]
-        resumptive_found_feature = ds["resumptive_found"]
-
-        if ANALYSE_GO:
-            # REMOVE SENTENCES WITH ADJECTIVES FOLLOWING COMPLEMENTISER
-            ds = ds[followed_by_adj_feature == False]
-
-            # REMOVE SENTENCES WITH NUMBERS FOLLOWING COMPLEMENTISER
-            ds = ds[followed_by_number_feature == False]
-
-            # REMOVE SENTENCES WITH NOUN PRECEDING COMPLEMENTISER
-            # This constraint can be removed.
-            # Given that a noun precedes it, does it tend to contain a potential resumptive pronoun?
-            # TODO: This should work through each clause in cycles.
-            # ds = ds[preceded_by_noun_feature == True]
-
-            # -- VIEW RESULTS -- 
-            num_preceded = sum(preceded_by_noun_feature)
-            percent_preceded = round(num_preceded / len(preceded_by_noun_feature) * 100,2)
-            percent_resumptive = (sum(resumptive_found_feature) / len(resumptive_found_feature)) * 100
-            print(f"DATASET: {title}")
-            print("Preceded by noun?")
-            print(preceded_by_noun_feature.value_counts())
-            print(f"Percentage Preceded: {percent_preceded}")
-            print(f"Percentage Containing Resumptive of the Noun-Preceding Comps: {percent_resumptive}")
+        print("Dataset: {}, {}".format(comp, region))
+        if comp == "go":
+            experiments = [
+                AnalyseNounBeforeGoExperiment(parsed_sentences),
+            ]
+        elif comp == "a":
+            experiments = [
+                AnalyseAExperiment(parsed_sentences),
+            ]
         else:
-            # -- TODO --
-            # remove any complementisers followed by a number
+            print("Invalid complementiser.")
+            continue
 
-            # FILTER: KEEP SENTENCES WITH NOUN PRECEDING COMPLEMENTISER
-            ds = ds[preceded_by_noun_feature]
+        runner = ExperimentRunner(experiments)
+        results = runner.run_all()
 
-            # apply a filter depending on whether a resumptive
-            # pronoun is present or not present
-            # this distinguishes aN (resumptive) from aL (no resumptive)
-            ds_resumptive = ds[resumptive_found_feature]
-            ds_nonresumptive = ds[~resumptive_found_feature]
-            view_data(title, ds_resumptive, ds_nonresumptive)
-
-
-def run_syntactic_analysis(ds):
-    is_followed_by_adjective = ds.apply(lemma_decorator, 
-                                   axis=1, 
-                                   args={ comp_analyser.is_followed_by_adjective  })
-
-    is_preceded_by_noun = ds.apply(lemma_decorator, 
-                                   axis=1, 
-                                   args={ comp_analyser.is_preceded_by_noun  })
-
-    is_followed_by_number = ds.apply(lemma_decorator, 
-                                   axis=1, 
-                                   args={ comp_analyser.is_followed_by_number  })
-
-    resumptive_found = ds.apply(lemma_decorator, 
-                                   axis=1, 
-                                   args={ comp_analyser.contains_resumptive  })
-
-    is_resumptive_found = resumptive_found.apply(lambda x: x['found'])
-    resumptive_token = resumptive_found.apply(lambda x: x['lemma'])
-
-    return {
-        "followed_by_adjective": is_followed_by_adjective,
-        "preceded_by_noun": is_preceded_by_noun,
-        "followed_by_number": is_followed_by_number,
-        "resumptive_found": is_resumptive_found,
-        "resumptive": resumptive_token
-    }
-
-def lemma_decorator(row, func):
-    lemmas = to_lemmas(row)
-    comp_index = len(lemmas['main'])
-    return func(lemmas["all"], comp_index)
-
-def view_data(title, resumptives, non_resumptives):
-    num_resumptives = len(resumptives)
-    num_non_resumptives = len(non_resumptives)
-    resumptives_found_as_percent = num_resumptives / (num_resumptives + num_non_resumptives) * 100
-    print("----")
-    print("Dataset: ", title)
-    print("Number of Resumptives: ", round(resumptives_found_as_percent,2))
-    print("Number of Non Resumptives: ", round(100.0 - resumptives_found_as_percent,2))
-    print(f"Resumptive Statistics: \n {resumptives['resumptive'].value_counts()}")
-
-    # We want to only include up to the full stop
-    # as we just want the current relevant sentence
-    # for the complementiser.
-    datasets = {
-        "RESUMPTIVE": resumptives,
-        "NON_RESUMPTIVE": non_resumptives
-    }
-    for title in datasets.keys():
-        counter = 0
-        ds = datasets[title]
-        print(f"\n{title} Examples")
-        for sentence in ds['full_sentence']:
-            counter += 1
-            print("\n")
-            print(sentence)
-            print("\n")
-
-            if counter == 5:
-                break
-    print("----\n")
-
+        for name, result in results.items():
+            print(f"{name}:")
+            for key, value in result.items():
+                print(f"  {key}: {value}")
+    
 if __name__ == "__main__":
     main()
